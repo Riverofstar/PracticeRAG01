b353c27af77669a5dab8fb4705a805e9db101799
Riverofstar <alsrb4367@gmail.com>
2024-10-20 11:34:46 +0900

    Update 실습02.py

--- 실습02.py
+++ 실습02.py
@@ -63,7 +63,7 @@ def main():
                     
                     # 카페 이름, 리뷰 수, 화살표를 같은 줄에 표시
                     st.write(f"- {cafe} (방문자리뷰: {review_count}) ", unsafe_allow_html=True)
-                    st.markdown(f"<a href='{naver_map_url}' target='_blank' style='text-decoration: none;'>➡️</a>", unsafe_allow_html=True)
+                    st.markdown(f"<a href='{naver_map_url}' target='_blank' style='text-decoration: none;'> ➡️</a>", unsafe_allow_html=True)
 
         elif st.session_state.service == 'chat_with_fairy':
             st.subheader("보드게임 요정에게 질문하기")
@@ -78,4 +78,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
